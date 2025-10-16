6f99ac0724d98e0928b74ed9fd4428600b37015b
FexcamiloUV <diaz.feliz@correounivalle.edu.co>
2025-10-15 23:26:11 -0500

    Llamado de calles

--- BASURAPP/src/app/home/home.page.ts
+++ BASURAPP/src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem } from '@ionic/angular/standalone';
+import { Calles } from 'src/interfaces/calles';
+import { Datos } from 'src/services/datos';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonItem],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  data: Calles[] = [];
+  datos = inject(Datos)
   constructor() {}
+  
+
+  ngOnInit(): void {
+    this.LlamarDatos();
+  }
+
+  async LlamarDatos() {
+    this.data = await this.datos.getDatos();
+  }
 }
